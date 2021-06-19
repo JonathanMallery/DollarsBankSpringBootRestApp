88196d8824d1225f9e271861313f4f87b54b5856
JonathanMallery <malleryjm@yahoo.com>
2021-06-19 11:50:33 -0500

    SpringBoot App

---
+++ src/main/java/com/cognixia/jumplus/springapp/dollarsbank/DollarsBankSpringBootRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jumplus.springapp.dollarsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarsBankSpringBootRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarsBankSpringBootRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognixia/jumplus/springapp/dollarsbank/DollarsBankSpringBootRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognixia.jumplus.springapp.dollarsbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollarsBankSpringBootRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
